36b298387dc03665c628fc7dd2194d7b7fd063ed
Volodymyr Ryba <vovaryba@gmail.com>
2021-11-13 22:23:51 +0200

    I made up the button for adding a transaction and fixed it in the corner

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import * as authOperations from './redux/auth/authOperations';
+import ButtonAddTransactions from './components/ButtonAddTransactions';
 import Chart from './components/Chart/Chart';
 import './App.css';
 
@@ -11,9 +12,12 @@ function App() {
     dispatch(authOperations.getCurrentUser());
   }, [dispatch]);
 
-  return <div className="App">Hello worlod!</div>;
+  return (
+    <div className="App">
+      Hello world!
+      <ButtonAddTransactions />
+    </div>
+  );
 }
 
-
 export default App;
-
