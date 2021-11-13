4c2cff0aa3275a724e59686c7a2bb523798c2eec
Anastasiia Solovei <74859753+Anastasia-Solovei@users.noreply.github.com>
2021-11-13 18:13:14 +0200

    updates App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import './App.css';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import Chart from './components/Chart/Chart';
+import * as authOperations from './redux/auth/authOperations';
+import './App.css';
+
 function App() {
-  return <div className="App"></div>;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(authOperations.getCurrentUser());
+  }, [dispatch]);
+
+  return <div className="App">Hello worlod!</div>;
 }
 
+
 export default App;
+
