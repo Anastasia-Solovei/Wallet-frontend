bbb9c2000c0bd5e3ec0f7282da221c361560f6c9
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-11-13 23:13:06 +0200

    fixes attributes at ButtonAddTransaction

--- src/components/ButtonAddTransactions/ButtonAddTransactions.js
+++ src/components/ButtonAddTransactions/ButtonAddTransactions.js
@@ -10,8 +10,8 @@ const ButtonAddTransactions = () => {
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <path d="M10 0V20" stroke="white" stroke-width="2" />
-        <path d="M0 10L20 10" stroke="white" stroke-width="2" />
+        <path d="M10 0V20" stroke="white" strokeWidth="2" />
+        <path d="M0 10L20 10" stroke="white" strokeWidth="2" />
       </svg>
     </button>
   );
