b977acf77f005db8fb2ebde69e054600f88d63ac
Halyna Sinko <galinasinko14@gmail.com>
2021-11-14 21:03:27 +0200

    daleted PublicRoute

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { Switch } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import * as authOperations from './redux/auth/authOperations';
 import LogInPage from './pages/LogInPage/LogInPage';
 import RegistrationPage from './pages/Registration';
 import DashboardPage from './pages/DashboardPage';
 import Chart from './components/Chart/Chart';
 import PrivateRoute from './components/PrivateRoute';
-import PublicRoute from './components/PublicRoute';
 import path from './routes_path';
 import './App.css';
 
@@ -21,20 +20,14 @@ function App() {
   return (
     <>
       <Switch>
-        <PublicRoute
-          path={path.registrationPage}
-          restricted
-          redirectTo={path.dashbordPage}
-        >
+        <Route path={path.registrationPage}>
           <RegistrationPage />
-        </PublicRoute>
-        <PublicRoute
-          path={path.logInPage}
-          restricted
-          redirectTo={path.dashbordPage}
-        >
+        </Route>
+
+        <Route path={path.logInPage}>
           <LogInPage />
-        </PublicRoute>
+        </Route>
+
         <PrivateRoute
           path={path.dashbordPage}
           exact
