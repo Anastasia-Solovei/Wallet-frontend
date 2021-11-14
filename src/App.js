83b5323d0c30370dd22b2522b1ce7d3b15b475b6
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-11-14 18:35:10 +0200

    revert all changes to the previous version of dev

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import './App.css';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import * as authOperations from './redux/auth/authOperations';
+import ButtonAddTransactions from './components/ButtonAddTransactions';
 import Chart from './components/Chart/Chart';
+import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(authOperations.getCurrentUser());
+  }, [dispatch]);
+
+  return (
+    <div className="App">
+      Hello world!
+      <ButtonAddTransactions />
+    </div>
+  );
 }
 
 export default App;
