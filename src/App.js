63a19aba53765aad14ee873c9ecc1e6f2f962319
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-11-14 18:24:40 +0200

    Revert "updates app.js"
    This reverts commit 14731bfc044a216b03d4f28f7423b7bd849e7fe2.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import * as authOperations from './redux/auth/authOperations';
-import ButtonAddTransactions from './components/ButtonAddTransactions';
-import Chart from './components/Chart/Chart';
 import Navigation from './components/Navigation/Navigation';
-import './App.css';
 
-function App() {
-  const dispatch = useDispatch();
+// import Chart from './components/Chart/Chart';
 
-  useEffect(() => {
-    dispatch(authOperations.getCurrentUser());
-  }, [dispatch]);
-
-  return (
-    <div className="App">
-      Hello world!
-      <ButtonAddTransactions />
-      <Navigation />
-    </div>
-  );
+export default function App() {
+  return <Navigation />;
 }
-
-export default App;
