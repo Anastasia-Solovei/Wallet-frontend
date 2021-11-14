91d49093d8987a08f9bedf23390be062e1feb3ee
Halyna Sinko <galinasinko14@gmail.com>
2021-11-14 19:12:46 +0200

    creates PrivateRoute and PublicRoute

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { Switch } from 'react-router-dom';
 import * as authOperations from './redux/auth/authOperations';
+import LogInPage from './pages/LogInPage/LogInPage';
+import RegistrationPage from './pages/Registration';
+import DashboardPage from './pages/DashboardPage';
 import Chart from './components/Chart/Chart';
+import PrivateRoute from './components/PrivateRoute';
+import PublicRoute from './components/PublicRoute';
+import path from './routes_path';
 import './App.css';
 
 function App() {
@@ -11,9 +18,33 @@ function App() {
     dispatch(authOperations.getCurrentUser());
   }, [dispatch]);
 
-  return <div className="App">Hello worlod!</div>;
+  return (
+    <>
+      <Switch>
+        <PublicRoute
+          path={path.registrationPage}
+          restricted
+          redirectTo={path.dashbordPage}
+        >
+          <RegistrationPage />
+        </PublicRoute>
+        <PublicRoute
+          path={path.logInPage}
+          restricted
+          redirectTo={path.dashbordPage}
+        >
+          <LogInPage />
+        </PublicRoute>
+        <PrivateRoute
+          path={path.dashbordPage}
+          exact
+          redirectTo={path.logInPage}
+        >
+          <DashboardPage />
+        </PrivateRoute>
+      </Switch>
+    </>
+  );
 }
 
-
 export default App;
-
