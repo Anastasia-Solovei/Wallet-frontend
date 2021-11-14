14731bfc044a216b03d4f28f7423b7bd849e7fe2
Anastasiia Solovei <74859753+Anastasia-Solovei@users.noreply.github.com>
2021-11-14 10:31:51 +0200

    updates app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import * as authOperations from './redux/auth/authOperations';
+import ButtonAddTransactions from './components/ButtonAddTransactions';
+import Chart from './components/Chart/Chart';
 import Navigation from './components/Navigation/Navigation';
+import './App.css';
 
-// import Chart from './components/Chart/Chart';
+function App() {
+  const dispatch = useDispatch();
 
-export default function App() {
-  return <Navigation />;
+  useEffect(() => {
+    dispatch(authOperations.getCurrentUser());
+  }, [dispatch]);
+
+  return (
+    <div className="App">
+      Hello world!
+      <ButtonAddTransactions />
+    <Navigation />
+    </div>
+  );
+
 }
+
+export default App;
+
