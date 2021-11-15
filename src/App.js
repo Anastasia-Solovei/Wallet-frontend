43860d0da013af2ec070f4f71e02a77f91999910
Inna Palchynska <InnaPalchynska@gmail.com>
2021-11-15 17:12:22 +0200

    dashbordPage => dashboardPage

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
         </Route>
 
         <ProtectedRoute
-          path={path.dashbordPage}
+          path={path.dashboardPage}
           exact
           redirectTo={path.logInPage}
         >
