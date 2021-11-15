b7dae0eb706b1661a1d3a4a85d2d4198fdd2f6d0
Alona-Suprun <74992052+Alona-Suprun@users.noreply.github.com>
2021-11-16 01:27:30 +0200

    add final styles

--- src/components/LogInForm/LogInForm.js
+++ src/components/LogInForm/LogInForm.js
@@ -2,26 +2,44 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import s from './LogInForm.module.css';
+import sprite from '../../images/svg_sprite.svg';
+
 import Button from '../Button/Button';
 
 const LogInForm = () => {
   return (
     <form className={s.form}>
+      <div className={s.titleArea}>
+        <h2 className={s.title}>Wallet</h2>
+        <svg className={s.walletIcon} width="30px" height="30px">
+          <use href={sprite + '#icon-wallet'}></use>
+        </svg>
+      </div>
       <label>
-        <input
-          className={s.inputEmail}
-          name="email"
-          type="email"
-          placeholder="E-mail"
-        />
+        <div className={s.inputArea}>
+          <input
+            className={s.input}
+            name="email"
+            type="email"
+            placeholder="E-mail"
+          />
+          <svg className={s.icon} width="24px" height="24px">
+            <use href={sprite + '#icon-email'}></use>
+          </svg>
+        </div>
       </label>
       <label>
-        <input
-          className={s.inputLock}
-          name="password"
-          type="password"
-          placeholder="Password"
-        />
+        <div className={s.inputArea}>
+          <input
+            className={s.input}
+            name="password"
+            type="password"
+            placeholder="Password"
+          />
+          <svg className={s.icon} width="24px" height="24px">
+            <use href={sprite + '#icon-password'}></use>
+          </svg>
+        </div>
       </label>
       <Button type="submit">Log in</Button>
       <Link to="/register" className={s.link}>
