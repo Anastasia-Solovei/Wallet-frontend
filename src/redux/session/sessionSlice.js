f1904932b5922d012a3184f3725bb87800603614
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-11-15 16:10:46 +0200

    adds error to initial state of store

--- src/redux/session/sessionSlice.js
+++ src/redux/session/sessionSlice.js
@@ -5,6 +5,7 @@ const initialState = {
   user: { name: null, email: null },
   token: null,
   isAuth: true,
+  error: null,
 };
 
 const sessionSlice = createSlice({
@@ -15,33 +16,36 @@ const sessionSlice = createSlice({
       state.global.isLoading = true;
     },
     [sessionOperations.register.fulfilled](state, action) {
-      state.user = action.payload.user;
+      state.session.user = action.payload.user;
       state.session.token = action.payload.token;
       state.session.isAuth = true;
       state.global.isLoading = false;
     },
-    [sessionOperations.register.rejected](state) {
+    [sessionOperations.register.rejected](state, action) {
       state.global.isLoading = false;
+      // state.session.error = action.payload.error;
     },
     [sessionOperations.logIn.pending](state) {
       state.global.isLoading = true;
     },
     [sessionOperations.logIn.fulfilled](state, action) {
-      state.user = action.payload.user;
+      state.session.user = action.payload.user;
       state.session.token = action.payload.token;
       state.session.isAuth = true;
       state.global.isLoading = false;
     },
     [sessionOperations.logIn.rejected](state) {
       state.global.isLoading = false;
+      // state.session.error = action.payload.error;
     },
     [sessionOperations.logOut.pending](state) {
       state.global.isLoading = true;
     },
     [sessionOperations.logOut.fulfilled](state) {
-      state.user = { name: null, email: null };
+      state.session.user = { name: null, email: null };
       state.session.token = null;
       state.session.isAuth = false;
+      state.session.error = null;
       state.global.isLoading = false;
       state.finance.totalBalance = 0;
     },
