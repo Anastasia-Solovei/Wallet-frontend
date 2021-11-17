72ead97234786f5d8ee424b2f153f8fbf6129ca5
prodanserega <74560554+prodanserega@users.noreply.github.com>
2021-11-17 11:27:55 +0200

    Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,28 @@
 import { useDispatch, useSelector } from 'react-redux';
+import sprite from '../../images/svg_sprite.svg';
 import style from './Header.module.css';
 
 const Header = () => {
   return (
     <header>
       <div className={style.header__inner}>
-        <div>
-          <svg class="logo" height="20" width="20">
-            <use href="../images/svg_sprite#icon-wallet"></use>
+        <div className={style.headerLogo}>
+          <svg className={style.headerIconLogo} width="30px" height="30px">
+            <use href={sprite + '#icon-wallet'}></use>
           </svg>
           <span className={style.logoName}>Wallet</span>
+        </div>
+        <div className={style.headerExit}>
+          <span className={style.userName}>Name</span>
+
+          {/* <span>|</span> */}
+          <button className={style.headerLogout}>
+            <svg className={style.headerIconExit} width="18px" height="18px">
+              <use href={sprite + '#icon-exit'}></use>
+            </svg>
+
+            {/* <span className={style.headerExit}>Exit</span> */}
+          </button>
         </div>
       </div>
     </header>
