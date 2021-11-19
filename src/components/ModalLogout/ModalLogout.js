1d610a516cda58b4c5ff937a5888b3c61424eaf0
Dariia Nikolaieva <74855191+DariiaNikolaieva@users.noreply.github.com>
2021-11-20 01:28:13 +0200

    devided into two components

--- src/components/ModalLogout/ModalLogout.js
+++ src/components/ModalLogout/ModalLogout.js
@@ -1,52 +1,27 @@
-import { useEffect } from 'react';
 import { useCallback } from 'react';
 import { useDispatch } from 'react-redux';
-import { createPortal } from 'react-dom';
 
-import { logOut } from '../../redux/session/sessionOperations'
-import { closeModalLogOut } from '../../redux/global/globalActions'
+import Modal from './Modal';
+
+import { logOut } from '../../redux/session/sessionOperations';
+import { closeModalLogOut } from '../../redux/global/globalActions';
 
 import s from './ModalLogout.module.css';
 
-const modalRoot = document.querySelector('#modal-root');
-
-export default function ModalLogout({onClose}) {
-    useEffect(() => {
-        const handleKeyDown = e => {
-          if (e.code === 'Escape') {
-            onClose();
-          }
-        };
-    
-        window.addEventListener('keydown', handleKeyDown);
-    
-        return () => {
-          window.removeEventListener('keydown', handleKeyDown);
-        };
-      }, [onClose]);
-    
-      const handleBackdropClick = event => {
-        if (event.currentTarget === event.target) {
-          onClose();
-        }
-      };
-
+export default function ModalLogout() {
   const dispatch = useDispatch();
 
   const onModalLogout = useCallback(() => {
-      dispatch(logOut());
+     dispatch(logOut());
   }, [dispatch]);
 
-  return createPortal(
-    <div className={s.modalBackdrop}>
-      <div className={s.modalContent}>
-        <p>Are you sure you want to exit?</p>
-        <div>
-          <button onClick={onModalLogout}>Yes</button>
-          <button onClick={() => dispatch(closeModalLogOut())}>No</button>
-        </div>
+  return (
+    <Modal closeModal={() => dispatch(closeModalLogOut())}>
+      <p>Are you sure you want to exit?</p>
+      <div>
+        <button onClick={onModalLogout}>Yes</button>
+        <button onClick={() => dispatch(closeModalLogOut())}>No</button>
       </div>
-    </div>,
-    modalRoot,
+    </Modal>
   );
 }
