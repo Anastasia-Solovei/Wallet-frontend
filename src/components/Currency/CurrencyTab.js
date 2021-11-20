e40433939de82084de7bb31ef20e6ebfeeef3e43
Inna Palchynska <InnaPalchynska@gmail.com>
2021-11-20 23:33:45 +0200

    bonus task

--- src/components/Currency/CurrencyTab.js
+++ src/components/Currency/CurrencyTab.js
@@ -7,6 +7,27 @@ import styles from './CurrencyTab.module.css';
 export default function CurrencyTable() {
   const [currency, setCurrency] = useState([]);
 
+  // useEffect(() => {
+  //   const fetchCurrency = async () => {
+  //     const data = await currencyApi.fetchCurrency();
+  //     const filtredCurrencies = [];
+  //     currencies.forEach(currency => {
+  //       data.forEach(element => {
+  //         parseInt(element.buy).toFixed(2);
+  //         if (element.ccy === currency) {
+  //           filtredCurrencies.push({
+  //             ccy: element.ccy,
+  //             buy: Number(element.buy).toFixed(2),
+  //             sale: Number(element.sale).toFixed(2),
+  //           });
+  //         }
+  //       });
+  //     });
+  //     setCurrency(filtredCurrencies);
+  //   };
+  //   fetchCurrency();
+  // }, []);
+
   useEffect(() => {
     const fetchCurrency = async () => {
       const data = await currencyApi.fetchCurrency();
@@ -24,8 +45,19 @@ export default function CurrencyTable() {
         });
       });
       setCurrency(filtredCurrencies);
+      localStorage.setItem('currency', JSON.stringify(filtredCurrencies));
+      localStorage.setItem('currencyTime', Date.now());
     };
-    fetchCurrency();
+    let currencyLS = JSON.parse(localStorage.getItem('currency'));
+    let currencyTime = JSON.parse(localStorage.getItem('currencyTime'));
+    if (!currencyLS) {
+      fetchCurrency();
+    }
+    if (Date.now() - currencyTime > 3600000) {
+      fetchCurrency();
+    } else {
+      setCurrency(currencyLS);
+    }
   }, []);
 
   return (
