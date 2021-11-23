e552493fd9811f0ab257d3c86895dc35643ae859
Alona-Suprun <74992052+Alona-Suprun@users.noreply.github.com>
2021-11-23 13:44:01 +0200

    add animation to LogInPage

--- src/pages/LogInPage/LogInPage.js
+++ src/pages/LogInPage/LogInPage.js
@@ -5,6 +5,7 @@ import sessionSelectors from '../../redux/session/sessionSelectors';
 import s from './LogInPage.module.css';
 import LogInForm from '../../components/LogInForm/LogInForm';
 import path from '../../routes_path';
+import Money from '../../components/Money/Money';
 
 const LogInPage = () => {
   const isAuth = useSelector(sessionSelectors.getIsAuth);
@@ -17,6 +18,7 @@ const LogInPage = () => {
     <>
       <div className={s.logInPageBlur}></div>
       <div className={s.logInPage}>
+        <Money />
         <div className={s.imgContent}>
           <div className={s.img}></div>
           <h1 className={s.pageHeading}>Finance App</h1>
