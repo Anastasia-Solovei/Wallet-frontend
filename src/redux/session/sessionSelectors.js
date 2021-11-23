96bf3f0bd8de3cf74d0fa0b840eab85346a029a9
Anastasiia Solovei <74859753+Anastasia-Solovei@users.noreply.github.com>
2021-11-23 02:54:56 +0200

    updates sessionSelectors

--- src/redux/session/sessionSelectors.js
+++ src/redux/session/sessionSelectors.js
@@ -4,10 +4,12 @@ const getUsername = state => state.session.user.name;
 
 const getUserEmail = state => state.session.user.email;
 
+const getError = state => state.session.error;
 
 const sessionSelectors = {
   getIsAuth,
   getUsername,
   getUserEmail,
+  getError,
 };
 export default sessionSelectors;
