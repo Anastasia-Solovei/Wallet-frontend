8b3adb7eefbbf0498d6bf8c99ded76a34423fa4b
MishaDreamer-web <shondayn@gmail.com>
2021-11-23 04:16:43 +0200

    fix-loader

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { lazy, Suspense, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { fetchCurrentUser } from './redux/session/sessionOperations';
@@ -7,12 +7,26 @@ import ProtectedRoute from './components/ProtectedRoute';
 import path from './routes_path';
 import LoaderSpinner from './components/Loader';
 
-import LogInPage from './pages/LogInPage/LogInPage';
-import RegistrationPage from './pages/RegistrationPage/RegistrationPage';
-import DashboardPage from './pages/DashboardPage/DashboardPage';
+// import LogInPage from './pages/LogInPage/LogInPage';
+// import RegistrationPage from './pages/RegistrationPage/RegistrationPage';
+// import DashboardPage from './pages/DashboardPage/DashboardPage';
 import './App.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
+const LogInPage = lazy(() =>
+  import('./pages/LogInPage/LogInPage.js' /* webpackChunkName: "login-page" */),
+);
+const RegistrationPage = lazy(() =>
+  import(
+    './pages/RegistrationPage/RegistrationPage.js' /* webpackChunkName: "registration-page" */
+  ),
+);
+const DashboardPage = lazy(() =>
+  import(
+    './pages/DashboardPage/DashboardPage.js' /* webpackChunkName: "dashboard-page" */
+  ),
+);
 
 function App() {
   const dispatch = useDispatch();
@@ -26,31 +40,37 @@ function App() {
 
   return (
     <>
-      {isLoading && <LoaderSpinner />}
+      {/* {isLoading && <LoaderSpinner />} */}
       {/* <LoaderSpinner /> */}
+      <Suspense fallback={<LoaderSpinner />}>
+        {isLoading ? (
+          <LoaderSpinner />
+        ) : (
+          <Switch>
+            <Route exact path="/">
+              <Redirect to={path.dashboardPage} />
+            </Route>
 
-      {/* {!isLoading && ( */}
-      <Switch>
-        <Route exact path="/">
-          <Redirect to={path.dashboardPage} />
-        </Route>
+            <Route path={path.registrationPage}>
+              <RegistrationPage />
+            </Route>
 
-        <Route path={path.registrationPage}>
-          <RegistrationPage />
-        </Route>
+            <Route path={path.logInPage}>
+              <LogInPage />
+            </Route>
 
-        <Route path={path.logInPage}>
-          <LogInPage />
-        </Route>
-        <ProtectedRoute
-          path={[path.dashboardPage, path.statistic, path.currency]}
-          exact
-          redirectTo={path.logInPage}
-        >
-          <DashboardPage />
-        </ProtectedRoute>
-      </Switch>
-      <ToastContainer position="top-right" autoClose={5000} closeOnClick />
+            <ProtectedRoute
+              path={[path.dashboardPage, path.statistic, path.currency]}
+              exact
+              redirectTo={path.logInPage}
+            >
+              <DashboardPage />
+            </ProtectedRoute>
+          </Switch>
+        )}
+
+        <ToastContainer position="top-right" autoClose={4000} closeOnClick />
+      </Suspense>
     </>
   );
 }
--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -2,16 +2,18 @@ import Loader from 'react-loader-spinner';
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 
 import styles from './Loader.module.css';
+// Add class name to loader
 
-const LoaderSpinner = ({ color = '#3f51b5' }) => {
+const LoaderSpinner = ({ color = 'rgba(36, 204, 167 ,0.85)' }) => {
   return (
     <div className={styles.container}>
       <Loader
+        className={styles.LoaderSpinner}
         type="ThreeDots"
         color={color}
         height={100}
         width={100}
-        // timeout={3000}
+        timeout={4000}
       />
     </div>
   );
