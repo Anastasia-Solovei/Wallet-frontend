e7ecab68f643757b441c27eadec308cab7e36003
Inna Palchynska <InnaPalchynska@gmail.com>
2021-11-23 23:46:40 +0200

    balance

--- src/components/Balance/Balance.js
+++ src/components/Balance/Balance.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect, useMemo } from 'react';
 // import { useSelector } from 'react-redux';
 import styles from './Balance.module.css';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { getTransactions } from '../../redux/transactions/transactionsSelectors';
+import { fetchTransactions } from '../../redux/transactions/transactionsOperations';
 
 const Balance = () => {
   //   const totalBalance = useSelector(state => state.finance.totalBalance);
-  const totalBalance = 24000;
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchTransactions());
+  }, [dispatch]);
+
+  const transactions = useSelector(getTransactions);
+
+  const data = useMemo(() => transactions, [transactions]);
+
+  const totalBalance = data.length === 0 ? 0 : data[data.length - 1].balance;
   return (
     <div className={styles.balance}>
       <h3 className={styles.title}>your balance</h3>
--- src/services/currencyApi.js
+++ src/services/currencyApi.js
@@ -2,7 +2,14 @@ import axios from 'axios';
 
 async function fetchCurrency() {
   const { data } = await axios.get(
-    'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5',
+    'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=11',
+    {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+        Authorization: ``,
+      },
+    },
   );
   return data;
 }
