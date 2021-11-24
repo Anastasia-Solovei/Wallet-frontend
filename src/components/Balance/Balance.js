48fb91f1a96a5f53aa390059ea53f967fd71c30d
Vitaliy Myts <74954880+Zombik007@users.noreply.github.com>
2021-11-24 05:38:52 +0200

    bag fix

--- src/components/Balance/Balance.js
+++ src/components/Balance/Balance.js
@@ -1,5 +1,4 @@
-
-import React, { useEffect, useState } from 'react';
+import React, { useMemo, useEffect, useState } from 'react';
 import { CSSTransition } from 'react-transition-group';
 
 // import { useSelector } from 'react-redux';
